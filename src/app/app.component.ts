6097caa14b9b3286d1b67b199918666b6d0ee486
Shrinivas Shah <shrinivasshah@Shrinivass-MacBook-Pro.local>
2021-07-04 19:45:30 +0530

    added everything

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
-
+import { WikipediaService } from './wikipedia.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'wsearch';
+  pages = [];
+
+  constructor(private wikipedia: WikipediaService) {}
+
+  onTerm(term: string) {
+    this.wikipedia.search(term).subscribe((response: any): any => {
+      this.pages = response.query.search;
+    });
+  }
 }
